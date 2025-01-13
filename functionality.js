0db0a820f37ee9847751e3a2930772b05420fa1a
priyalhotmail <priyal_hdp@hotmail.com>
2025-01-13 11:30:44 +0100

    adding comments

--- functionality.js
+++ functionality.js
@@ -1,7 +1,10 @@
+// Function to add two numbers passing as parameters
 function addNumbers (no1, no2){
     console.log((no1 + no2));
 }
 
+// Define total variable to store the calculated value
 const total = addNumbers(12, 11);
 
+// Print the total
 console.log(total);
